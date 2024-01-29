7eb751423a0d94636edad61e290787b6276a4e1d
StanislavGautiero <theleader@bk.ru>
2024-01-29 20:21:45 +0200

    Project 1 - beginner lvl

--- 001-025/day-001/band_name_generator.py
+++ 001-025/day-001/band_name_generator.py
@@ -1,1 +1,6 @@
-print("Welcome to \nPython 100-to-Pro")
+# Band name generator project - Beginner lvl
+print("What's the name of the city you grew up in?")
+city = input()
+print("What's your pet's name?")
+pet = input()
+print(f"Your band name could be {city} {pet}")

4c4628999f7e957c7545742917191344ca0e7626
StanislavGautiero <theleader@bk.ru>
2024-02-06 22:09:55 +0200

    Project 6 - Escaping the maze - beginner lvl. External project location

---
+++ 001-025/day-006/project_day6/escaping_the_maze.py
@@ -0,0 +1,21 @@
+# the task is located on
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# the code for task will be commented to avoid errors on IDE
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while front_is_clear():
+#     move()
+# turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
